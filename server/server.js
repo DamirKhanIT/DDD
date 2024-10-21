348b56bca8fe0c847047be63b54a5b44d1a6c2fd
DamirKhanIT <damirmusekenov4@mail.com>
2024-10-21 23:49:37 +0600

    first commit

--- server/server.js
+++ server/server.js
@@ -43,5 +43,5 @@ app.post('/api/users/register', async (req, res) => {
 });
 
 // Запуск сервера
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
